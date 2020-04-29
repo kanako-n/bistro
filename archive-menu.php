ba3168355caf959617952e54fba8a164181fafa8
Kanako Nagiri <kanako22n@gmail.com>
2020-04-29 22:52:43 +0900

    カスタム投稿、フィールド、タクソノミー メニュー作成

--- archive-menu.php
+++ archive-menu.php
@@ -2,18 +2,53 @@
     
     <h2 class="pageTitle">メニュー<span>MENU</span></h2>
     <?php get_template_part( 'template-parts/breadcrumb' ); ?>
-    
+<?php
+$kinds = get_terms(array(
+    'taxonomy' => 'kind',
+    'order' => 'DESC'
+));
+if ( !empty($kinds) ):
+?>
+<div class="pageNav">
+    <ul>
+        <?php foreach ( $kinds as $kind): ?>
+            <li><a href="<?php echo get_term_link($kind); ?>"><?php echo $kind->name ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+<?php endif; ?>
     <main class="main">
+<?php
+$kinds = get_terms(array(
+    'taxonomy' => 'kind',
+    'order' => 'DESC'
+));
+if ( !empty($kinds) ):
+?>
+        <?php foreach ( $kinds as $kind ): ?>
         <section class="sec">
             <div class="container">
                 <div class="sec_header">
-                    <h2 class="title title-jp">フード</h2>
-                    <span class="title title-en">FOOD</span>
+                    <h2 class="title title-jp"><?php echo $kind->name; ?></h2>
+                    <span class="title title-en"><?php echo strtoupper($kind->slug); ?></span>
                 </div>
                 <div class="row justify-content-center">
 <?php
-if( have_posts() ):
-    while( have_posts() ):the_post();
+$args = array(
+    'post_type' => 'menu',
+    'post_per_page' => -1,
+);
+$taxquerysp = array('relation' => 'AND' );
+$taxquerysp[] = array(
+    'taxonomy' => 'kind',
+    'terms' =>$kind->slug,
+    'field' => 'slug',
+);
+$args['tax_query'] = $taxquerysp;
+
+$the_query = new WP_Query($args);
+if ( $the_query->have_posts() ):
+    while( $the_query->have_posts() ): $the_query->the_post();
 ?>
                     <div class="col-md-3">
                         <?php get_template_part( 'template-parts/loop', 'menu' ); ?>
@@ -25,6 +60,8 @@ endif;
                 </div>
             </div>
         </section>
+<?php endforeach; ?>
+<?php endif; ?>
     </main>
     
 <?php get_footer(); ?>
--- taxonomy.php
+++ taxonomy.php
@@ -1,229 +1,33 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>サンプルサイト</title>
-    <link href="./assets/css/styles.min.css" rel="stylesheet">
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="./assets/js/main.js"></script>
-</head>
-<body>
-    <header class="header">
-        <div class="header_inner">
-            <div class="header_logo">
-                <h1><a href="/"><img src="./assets/img/common/logo@2x.png" alt="BISTRO CALME"></a></h1>
-            </div>
-
-            <div class="header_desc"><p>サイトのキャッチフレーズ</p></div>
-
-            <form class="header_search">
-                <input type="text" placeholder="キーワードを入力">
-                <i class="fas fa-search"></i>
-            </form>
-        </div>
-
-        <div class="header_links">
-            <nav class="gnav">
-                <ul class="">
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#">わたしたちについて</a></li>
-                    <li><a href="#">アクセス</a></li>
-                    <li><a href="#">最新情報</a></li>
-                    <li><a href="#">お問い合わせ</a></li>
-                </ul>
-            </nav>
-
-            <ul class="header_sns">
-                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            </ul>
-        </div>
-
-        <svg class="header_menu" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" height="30" viewBox="0 0 30 30">
-            <defs><clipPath id="clip-path"><rect width="30" height="30" fill="none"/></clipPath></defs>
-            <g clip-path="url(#clip-path)">
-                <rect class="header_border header_border-1" width="30" height="2" transform="translate(0 0)"/>
-                <rect class="header_border header_border-2" width="30" height="2" transform="translate(0 10)"/>
-                <rect class="header_border header_border-3" width="30" height="2" transform="translate(0 20)"/>
-            </g>
-        </svg>
-    </header>
+<?php get_header(); ?>
 
     <h2 class="pageTitle">メニュー<span>MENU</span></h2>
-
+    <?php get_template_part( 'template-parts/breadcrumb' ); ?>
     <main class="main">
+<?php
+$kind_slug = get_query_var('kind');
+$kind = get_term_by('slug', '$kind_slug', 'kind');
+?>
         <section class="sec">
             <div class="container">
                 <div class="sec_header">
-                    <h2 class="title title-jp">フード</h2>
-                    <span class="title title-en">FOOD</span>
+                    <h2 class="title title-jp"><?php single_term_title( '' ); ?></h2>
+                    <span class="title title-en"><?php echo $kind->name; ?></span>
                 </div>
                 <div class="row justify-content-center">
-
+<?php
+if( have_posts() ):
+    while( have_posts() ):the_post();
+?>
                     <div class="col-md-3">
-                        <section class="menu">
-                            <a href="#">
-                                <figure class="menu_pic">
-                                    <img src="./assets/img/menu/menu-1@2x.jpg" alt="">
-                                </figure>
-                                <h3 class="menu_title">メニュー名が入ります</h3>
-                                <p class="menu_price">800円</p>
-                                <div class="menu_desc">
-                                    <p>
-                                        概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                    </p>
-                                </div>
-                            </a>
-                        </section>
+                        <?php get_template_part( 'template-parts/loop', 'menu' ); ?>
                     </div>
-
-                    <div class="col-md-3">
-                        <section class="menu">
-                            <a href="#">
-                                <figure class="menu_pic">
-                                    <img src="./assets/img/menu/menu-2@2x.jpg" alt="">
-                                </figure>
-                                <h3 class="menu_title">メニュー名が入ります</h3>
-                                <p class="menu_price">800円</p>
-                                <div class="menu_desc">
-                                    <p>
-                                        概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                    </p>
-                                </div>
-                            </a>
-                        </section>
-                    </div>
-
-                    <div class="col-md-3">
-                        <section class="menu">
-                            <a href="#">
-                                <figure class="menu_pic">
-                                    <img src="./assets/img/menu/menu-3@2x.jpg" alt="">
-                                </figure>
-                                <h3 class="menu_title">メニュー名が入ります</h3>
-                                <p class="menu_price">800円</p>
-                                <div class="menu_desc">
-                                    <p>
-                                        概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                    </p>
-                                </div>
-                            </a>
-                        </section>
-                    </div>
-
-                    <div class="col-md-3">
-                        <section class="menu">
-                            <a href="#">
-                                <figure class="menu_pic">
-                                    <img src="./assets/img/menu/menu-4@2x.jpg" alt="">
-                                </figure>
-                                <h3 class="menu_title">メニュー名が入ります</h3>
-                                <p class="menu_price">800円</p>
-                                <div class="menu_desc">
-                                    <p>
-                                        概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                    </p>
-                                </div>
-                            </a>
-                        </section>
-                    </div>
-
-                    <div class="col-md-3">
-                        <section class="menu">
-                            <a href="#">
-                                <figure class="menu_pic">
-                                    <img src="./assets/img/menu/menu-5@2x.jpg" alt="">
-                                </figure>
-                                <h3 class="menu_title">メニュー名が入ります</h3>
-                                <p class="menu_price">800円</p>
-                                <div class="menu_desc">
-                                    <p>
-                                        概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                    </p>
-                                </div>
-                            </a>
-                        </section>
-                    </div>
-
-                    <div class="col-md-3">
-                        <section class="menu">
-                            <a href="#">
-                                <figure class="menu_pic">
-                                    <img src="./assets/img/menu/menu-6@2x.jpg" alt="">
-                                </figure>
-                                <h3 class="menu_title">メニュー名が入ります</h3>
-                                <p class="menu_price">800円</p>
-                                <div class="menu_desc">
-                                    <p>
-                                        概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                    </p>
-                                </div>
-                            </a>
-                        </section>
-                    </div>
-
-                    <div class="col-md-3">
-                        <section class="menu">
-                            <a href="#">
-                                <figure class="menu_pic">
-                                    <img src="./assets/img/menu/menu-7@2x.jpg" alt="">
-                                </figure>
-                                <h3 class="menu_title">メニュー名が入ります</h3>
-                                <p class="menu_price">800円</p>
-                                <div class="menu_desc">
-                                    <p>
-                                        概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                    </p>
-                                </div>
-                            </a>
-                        </section>
-                    </div>
-
-                    <div class="col-md-3">
-                        <section class="menu">
-                            <a href="#">
-                                <figure class="menu_pic">
-                                    <img src="./assets/img/menu/menu-8@2x.jpg" alt="">
-                                </figure>
-                                <h3 class="menu_title">メニュー名が入ります</h3>
-                                <p class="menu_price">800円</p>
-                                <div class="menu_desc">
-                                    <p>
-                                        概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                    </p>
-                                </div>
-                            </a>
-                        </section>
-                    </div>
-
+<?php
+    endwhile;
+endif;
+?>
                 </div>
             </div>
         </section>
     </main>
 
-    <div class="pagetop js-pagetop"><i class="fas fa-angle-up"></i>PAGE TOP</div>
-
-    <footer class="footer">
-        <div class="container">
-            <div class="footer_inner">
-                <nav>
-                    <ul>
-                        <li><a href="#">HOME</a></li>
-                        <li><a href="#">わたしたちについて</a></li>
-                        <li><a href="#">アクセス</a></li>
-                        <li><a href="#">最新情報</a></li>
-                        <li><a href="#">お問い合わせ</a></li>
-                    </ul>
-                </nav>
-                <div class="footer_copyright">
-                    <small>&copy; BISTRO CALME All rights reserved.</small>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-</body>
-</html>
+<?php get_footer(); ?>
